eeef3e3ac03846d9c073726f5fe2009d7d1f9890
Clément Grennerat <clement.grennerat@clgr.io>
2025-10-09 10:25:52 +0200

    Adding tracker script.

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
+  <script async src="https://ackee.clgr.io/jquerydom.js" data-ackee-server="https://ackee.clgr.io" data-ackee-domain-id="47efb994-ddae-49a6-b9ff-c433ae614ede"></script>
   <HomeHeader/>
   <AboutMe/>
   <ProfesionalExperience/>
